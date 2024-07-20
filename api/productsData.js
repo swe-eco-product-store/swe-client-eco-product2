e8f5cf3afd03421a79f6d340a5d44f399f6b348c
Libannur <99373712+Libannur@users.noreply.github.com>
2024-07-20 12:03:01 -0500

    re-wrote api calls

---
+++ api/productsData.js
@@ -0,0 +1,125 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// GET KITCHEN PRODUCTS
+const getKitchenProducts = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products?category_id=1`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// GET BATH PRODUCTS
+const getBathProducts = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products?category_id=2`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// GET PETS PRODUCTS
+const getPetProducts = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products?category_id=3`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// GET SINGLE PRODUCT
+const getSingleProduct = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// DELETE SINGLE PRODUCT
+const deleteSingleProduct = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// CREATE PRODUCT
+const createProducts = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// UPDATE PRODUCTS
+
+const updateProducts = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/products/${payload.id}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getBathProducts,
+  getKitchenProducts,
+  getPetProducts,
+  getSingleProduct,
+  createProducts,
+  deleteSingleProduct,
+  updateProducts,
+};
