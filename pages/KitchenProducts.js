08482a2d6137618b56199fb84af3f5e9b6a336c4
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-07-28 18:56:58 -0500

    added button to all product pages and fixed refresh

--- pages/KitchenProducts.js
+++ pages/KitchenProducts.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 import { getKitchenProducts } from '../api/productsData';
 import ProductCard from '../components/ProductCard';
 
 function ViewKitchenProducts() {
   const [kitchenProducts, setKitchenProducts] = useState([]);
+  const router = useRouter();
 
   const getAllKitchenProducts = () => {
     getKitchenProducts()
@@ -19,8 +22,15 @@ function ViewKitchenProducts() {
     getAllKitchenProducts();
   }, []);
 
+  const handleAddProduct = () => {
+    router.push('/newProducts');
+  };
+
   return (
     <div>
+      <div className="Button text-center my-4">
+        <Button variant="primary" onClick={handleAddProduct}>Add A Product</Button>
+      </div>
       {kitchenProducts.map((product) => (
         <ProductCard key={product.id} Obj={product} onUpdate={getAllKitchenProducts} />
       ))}
