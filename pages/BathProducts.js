e435b683f33c12e2d229944ac304963a9dd5b8c6
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-07-28 19:10:19 -0500

    bath cards styling

--- pages/BathProducts.js
+++ pages/BathProducts.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 import { getBathProducts } from '../api/productsData';
 import ProductCard from '../components/ProductCard';
 
 function ViewBathProducts() {
   const [bathProducts, setBathProducts] = useState([]);
+  const router = useRouter();
 
   const getAllBathProducts = () => {
     getBathProducts()
@@ -19,11 +22,22 @@ function ViewBathProducts() {
     getAllBathProducts();
   }, []);
 
+  const handleAddProduct = () => {
+    router.push('/newProducts');
+  };
+
   return (
-    <div>
-      {bathProducts.map((product) => (
-        <ProductCard key={product.id} Obj={product} onUpdate={getAllBathProducts} />
-      ))}
+    <div className="container">
+      <div className="Button text-center my-4">
+        <Button variant="primary" onClick={handleAddProduct}>Add A Product</Button>
+      </div>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+        {bathProducts.map((product) => (
+          <div className="col" key={product.id}>
+            <ProductCard Obj={product} onUpdate={getAllBathProducts} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
