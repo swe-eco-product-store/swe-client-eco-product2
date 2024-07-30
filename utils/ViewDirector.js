40c4e8259618ad18b0b37f4c2b15e00ea4d42cfe
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-07-29 19:59:43 -0500

    outline on product cards

--- utils/ViewDirector.js
+++ utils/ViewDirector.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import { useAuth } from './context/authContext';
 import Loading from '../components/Loading';
 import Signin from '../components/Signin';
@@ -10,17 +11,17 @@ const ViewDirectorBasedOnUserAuthStatus = ({
   component: Component, pageProps, hideNavbar, hideFooter,
 }) => {
   const { user, userLoading, updateUser } = useAuth();
+  const router = useRouter();
+  const isHomePage = router.pathname === '/';
 
-  // if user state is null, then show loader
   if (userLoading) {
     return <Loading />;
   }
 
-  // what the user should see if they are logged in
   if (user) {
     return (
       <>
-        {!hideNavbar && <NavBar />} {/* conditionally render Navbar */}
+        {!hideNavbar && <NavBar />}
         <div className="container">
           {'valid' in user ? (
             <RegisterForm user={user} updateUser={updateUser} />
@@ -28,7 +29,7 @@ const ViewDirectorBasedOnUserAuthStatus = ({
             <Component {...pageProps} />
           )}
         </div>
-        {!hideFooter && <Footer />} {/* conditionally render Footer */}
+        {!hideFooter && !isHomePage && <Footer />}
       </>
     );
   }
