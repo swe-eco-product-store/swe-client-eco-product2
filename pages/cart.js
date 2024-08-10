22e40e14925704199c365b23c51a25b55ad04948
Libannur <99373712+Libannur@users.noreply.github.com>
2024-08-10 09:42:12 -0500

    centered checkout out btn

--- pages/cart.js
+++ pages/cart.js
@@ -27,11 +27,11 @@ const CartPage = () => {
   };
 
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
       <h1>My Cart</h1>
       <ViewCart cart={cart} removeFromCart={removeFromCart} />
       <h2>Total: ${total.toFixed(2)}</h2>
-      <Link passHref href="/checkoutpage">
+      <Link style={{textAlign: 'center'}} passHref href="/checkoutpage">
         <Button size="lg">Checkout</Button>
       </Link>
     </div>
